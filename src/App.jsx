872ff52b041a8e26375271ed556abf9947ccd80d
Trung Khiêm <trungkhiem1412@gmail.com>
2022-10-29 12:20:25 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h3 className="text-center">Nguyễn Trung Khiêm</h3>
+			</header>
+		</div>
+	);
+}
+
+export default App;
